8f27a97a1b04b2fed8e4fdcfb5d8a98c0b363bfd
Sebastian <sebastian.szneider@student.put.poznan.pl>
2017-02-07 10:25:04 +0100

    wstep do rozwijania query wykorzystujac cossim

--- query/query_expander.py
+++ query/query_expander.py
@@ -5,31 +5,11 @@ from typing import List
 class QueryExpander:
     def __init__(self, model: LanguageModel):
         self.model = model
-
-    def p7(self, word1: str, word2: str, topn: int):
-        similars = self.model.find_most_similar_words(word2, topn)
-        numerator = self.model.similarity(word1, word2)
-        denominator = 0
-        for word3, score in similars:
-            denominator += self.model.similarity(word3, word2)
-        if denominator != 0:
-            return numerator/denominator
-
-    def p8(self, word: str, query: List[str], topn: int):
-        prob = 0
-        query_length = len(query)
-        for term in query:
-            term_count = 0
-            for term2 in query:
-                if term2 == term:
-                    term_count += 1
-            prob += self.p7(word, term, topn) * term_count/query_length
-        return prob
-
-    def ExpandQuery(self, query: List[str], limit = 20):
+    def ExpandQuery(self, query: List[str], topn = 1):
         expanded = list(query)
-        tmp = dict()
         for term in query:
-            tmp[term] = self.p8(term, query, limit)
-        #return expanded
-        return tmp
+            most_similars = self.model.find_most_similar_words(term, topn)
+            for similar, score in most_similars:
+                if similar not in expanded:
+                    expanded.append(similar)
+        return expanded
