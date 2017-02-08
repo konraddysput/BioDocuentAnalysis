69d195e38e78e8e061bc3c322a1336e4f8a97ff9
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2017-02-08 10:53:03 +0100

    Add generating sums cache with C++

--- dataloader/model.py
+++ dataloader/model.py
@@ -13,22 +13,15 @@ from dataloader.sim_regression import SimRegression
 
 class LanguageModel:
     def __init__(self, path: str, classifier: SimRegression = None):
-        words: np.ndarray = pd.read_csv(path, sep=' ', quoting=3, header=None, usecols=(0,)).values.squeeze()
-
-        self._dictionary: Dict[str, int] = dict(zip(words, range(len(words))))
+        self._words: List[str] = pd.read_csv(
+            path, sep=' ', quoting=3, header=None, usecols=(0,), na_filter=False).values.squeeze().tolist()
         self._vectors: np.ndarray = pd.read_csv(path, sep=' ', quoting=3, header=None, usecols=range(1, 51),
-                                                dtype=np.float32).values
+                                                na_filter=False, dtype=np.float32).values
 
-        self.classifier: SimRegression = classifier
-        self._similarity = CppSemanticSimilarity(self._vectors)
+        self._similarity = CppSemanticSimilarity(self._words, self._vectors)
 
-    def similarity(self, word1: str, word2: str):
-        if word1 in self._dictionary and word2 in self._dictionary:
-            return self._similarity.calculate_similarity(self._vectors[self._dictionary[word1]],
-                                                         self._vectors[self._dictionary[word2]])
-        return None
+    def generate_sums_cache(self):
+        self._similarity.generate_sums_cache()
 
     def find_most_similar_words(self, word: str, number_of_results: int) -> List[Tuple[str, float]]:
-        print(self._similarity.find_most_similar_words(self._dictionary[word], number_of_results))
-
-        return None
+        return self._similarity.find_most_similar_words(word, number_of_results)
