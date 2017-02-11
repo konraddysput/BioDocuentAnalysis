81b693bb88b60d7928d3e61850e6da2049760278
Sebastian <sebastian.szneider@student.put.poznan.pl>
2017-02-11 16:19:56 +0100

    wzory 7 i 8, cosinus-sigmoid

--- query/query_expander.py
+++ query/query_expander.py
@@ -5,11 +5,31 @@ from typing import List
 class QueryExpander:
     def __init__(self, model: LanguageModel):
         self.model = model
-    def ExpandQuery(self, query: List[str], topn = 1):
-        expanded = list(query)
+
+    def p7(self, word1: str, word2: str, topn: int):
+        similars = self.model.find_most_similar_words(word2, topn)
+        numerator = self.model.similarity(word1, word2)
+        denominator = 0
+        for word3, score in similars:
+            denominator += self.model.similarity(word3, word2)
+        if denominator != 0:
+            return numerator/denominator
+
+    def p8(self, word: str, query: List[str], topn: int):
+        prob = 0
+        query_length = len(query)
         for term in query:
-            most_similars = self.model.find_most_similar_words(term, topn)
-            for similar, score in most_similars:
-                if similar not in expanded:
-                    expanded.append(similar)
-        return expanded
+            term_count = 0
+            for term2 in query:
+                if term2 == term:
+                    term_count += 1
+            prob += self.p7(word, term, topn) * term_count/query_length
+        return prob
+
+    def ExpandQuery(self, query: List[str], limit = 20):
+        expanded = list(query)
+        tmp = dict()
+        for term in query:
+            tmp[term] = self.p8(term, query, limit)
+        #return expanded
+        return tmp
--- tests/sandbox.py
+++ tests/sandbox.py
@@ -1,23 +1,13 @@
 from dataloader.model import LanguageModel
-from dataloader.cosine_similarity import CosineSimilarity
-from dataloader.p_function import PFunction
+from dataloader.cosine_sigmoid_similarity import CosineSigmoidSimilarity
+from dataloader.euclidian_similarity import EuclideanSimilarity
 from query.query_expander import QueryExpander
 import random
 
-def semantic_sim(model: LanguageModel, word1: str, word2: str):
-    numerator = model.similarity(word1, word2)
-    denominator = 0
-    for word3, index in model._dictionary.items():
-        denominator += model.similarity(word3, word2)
-    if denominator != 0:
-        return numerator/denominator
-    return None
-
 if __name__ == '__main__':
     random.seed(2017)
-    cosine_similarity = CosineSimilarity()
+    cosine_sigmoid_similarity = CosineSigmoidSimilarity(5, 1)
     model = LanguageModel('glove.6B.50d.txt')
-    p_func = PFunction(model, cosine_similarity)
-    model.classifier = cosine_similarity
+    model.classifier = cosine_sigmoid_similarity
     query_expander = QueryExpander(model)
-    print(query_expander.ExpandQuery(['how', 'to', 'wash', 'a', 'cat']))
+    print(query_expander.ExpandQuery(['dog', 'hates', 'cat']))
