0f7808693bc28f2c6f98706ad74bdf23374b43a1
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2017-02-21 12:50:08 +0100

    Update accelerator dependency

--- tests/sandbox.py
+++ tests/sandbox.py
@@ -15,6 +15,11 @@ def semantic_sim(model: LanguageModel, word1: str, word2: str):
 if __name__ == '__main__':
     random.seed(2017)
     model = LanguageModel('glove.6B.50d.txt')
-    print(model.find_most_similar_words(["beaver", "lives", "water"], 10))
+
+    with open('QUERIES') as file:
+        for line in file:
+            print(model.find_most_similar_words(line.rstrip('\n').lower().split(' '), 10))
+
+    # print(model.find_most_similar_words(["beaver", "lives", "water"], 10))
     # model.generate_sums_cache()
     # print(model.find_most_similar_words(["dog"], 10))
