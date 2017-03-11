11f451e4cbf926bcddc560bddc071f775afc4daf
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2017-03-11 21:53:44 +0100

    Add passing centroids path in CLI

--- queryexpander/__main__.py
+++ queryexpander/__main__.py
@@ -14,8 +14,9 @@ def cli():
 @click.argument('vocabulary_path', required=True, type=click.Path(exists=True, dir_okay=False))
 @click.argument('vocabulary_length', required=True, type=int)
 @click.argument('sums_cache_file', required=True, type=str)
-def generate_cache(vocabulary_path: str, vocabulary_length: int, sums_cache_file: str):
-    query_expander = QueryExpander(vocabulary_path, vocabulary_length, sums_cache_file)
+@click.argument('centroids_file_path', required=True, type=str)
+def generate_cache(vocabulary_path: str, vocabulary_length: int, sums_cache_file: str, centroids_file_path: str):
+    query_expander = QueryExpander(vocabulary_path, vocabulary_length, sums_cache_file, centroids_file_path)
     query_expander.generate_sums_cache()
 
 
@@ -37,9 +38,10 @@ def generate_centroids(vocabulary_path: str, vocabulary_length: int, sums_cache_
 @click.argument('vocabulary_length', required=True, type=int)
 @click.argument('number_of_additional_words', required=True, type=int)
 @click.argument('sums_cache_file', required=True, type=str)
+@click.argument('centroids_file_path', required=True, type=str)
 def expand(query: str, vocabulary_path: str, vocabulary_length: int, number_of_additional_words: int,
-           sums_cache_file: str):
-    query_expander = QueryExpander(vocabulary_path, vocabulary_length, sums_cache_file)
+           sums_cache_file: str, centroids_file_path: str):
+    query_expander = QueryExpander(vocabulary_path, vocabulary_length, sums_cache_file, centroids_file_path)
     print(query_expander.find_most_similar_words(query.rstrip('\n').lower().split(' '), number_of_additional_words))
 
 
@@ -50,9 +52,10 @@ def expand(query: str, vocabulary_path: str, vocabulary_length: int, number_of_a
 @click.argument('vocabulary_length', required=True, type=int)
 @click.argument('number_of_additional_words', required=True, type=int)
 @click.argument('sums_cache_file', required=True, type=str)
+@click.argument('centroids_file_path', required=True, type=str)
 def expand_file(input_file_path: str, output_file_path: str, vocabulary_path: str, vocabulary_length: int,
-                number_of_additional_words: int, sums_cache_file: str):
-    query_expander = QueryExpander(vocabulary_path, vocabulary_length, sums_cache_file)
+                number_of_additional_words: int, sums_cache_file: str, centroids_file_path: str):
+    query_expander = QueryExpander(vocabulary_path, vocabulary_length, sums_cache_file, centroids_file_path)
 
     with open(input_file_path) as input_file:
         with open(output_file_path, 'w') as output_file:
