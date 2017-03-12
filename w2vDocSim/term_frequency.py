04a18f452b989e325c712d693535f24cf39915d3
Konrad Dysput <konrad.dysput@gmail.com>
2017-03-12 22:40:25 +0100

    TFIDF refactor

--- w2vDocSim/term_frequency.py
+++ w2vDocSim/term_frequency.py
@@ -1,41 +1,48 @@
 from math import log
 
-def total_words_in_document(document):
-    count = 0
-    for item in document:
-        count += len(item.split())
-    return count
 
-def count_word_in_documents(frequency, documents):
-    count = 0
-    for string in documents:
-        for word in string.split():
-            if(word == frequency):
-                count += 1
-    return count
+class TermFrequency:
+    documents = []
 
-def calculate_term_frequency(word, documents):
-    return count_word_in_documents(word, documents) / total_words_in_document(documents)
+    def __init__(self, total_documents):
+        self.documents = total_documents
 
-def calculate_inverse_document_frequenct(word, documents):
-    return log(len(documents) / word_in_documents(word, documents))
+    def total_words_in_document(self):
+        count = 0
+        for item in self.documents:
+            count += len(item.split())
+        return count
 
-def word_in_documents(word, documents):
-    return [word in s.split(' ') for s in documents].count(True)
+    def count_word_in_documents(self, frequency):
+        count = 0
+        for string in self.documents:
+            for word in string.split():
+                if(word == frequency):
+                    count += 1
+        return count
+
+    def calculate_term_frequency(self, word):
+        return self.count_word_in_documents(word) / self.total_words_in_document()
+
+    def calculate_inverse_document_frequency(self, word):
+        return log(len(self.documents) / self.word_in_documents(word))
+
+    def word_in_documents(self, word):
+        return [word in s.split(' ') for s in self.documents].count(True)
 
 # if __name__ == '__main__':
 #     documents = [" to jest test ktory sprawdzi czy slowo jest zawarte w stringu", "jem banana", "kupie  test test krakersy",
 #                  "testcik"]
-#
-#     count = count_word_in_documents("test", documents)
+#     frequence = TermFrequency(documents)
+#     count = frequence.count_word_in_documents("test")
 #     print(count)
 #
 #
-#     count = word_in_documents("test", documents)
+#     count = frequence.word_in_documents("test")
 #     print(count)
 #
-#     result = calculate_inverse_document_frequenct("test", documents)
+#     result = frequence.calculate_inverse_document_frequency("test")
 #     print(result)
 #
-#     result = calculate_term_frequency("test", documents)
+#     result = frequence.calculate_term_frequency("test")
 #     print(result)
