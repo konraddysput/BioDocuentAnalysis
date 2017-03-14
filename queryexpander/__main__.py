7cf5749a3485a81de032c4f1c87acd3023cf32e6
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2017-03-14 22:43:45 +0100

    Fix code for file query expansion

--- queryexpander/__main__.py
+++ queryexpander/__main__.py
@@ -40,7 +40,7 @@ def expand(query: str, vocabulary_path: str, vocabulary_length: int, number_of_a
 
 @click.command()
 @click.argument('input_file_path', required=True, type=click.Path(exists=True, dir_okay=False))
-@click.argument('output_file_path', required=True, type=click.Path(exists=True, dir_okay=False))
+@click.argument('output_file_path', required=True, type=click.Path(dir_okay=False))
 @click.argument('vocabulary_path', required=True, type=click.Path(exists=True, dir_okay=False))
 @click.argument('vocabulary_length', required=True, type=int)
 @click.argument('number_of_additional_words', required=True, type=int)
@@ -53,8 +53,12 @@ def expand_file(input_file_path: str, output_file_path: str, vocabulary_path: st
     with open(input_file_path) as input_file:
         with open(output_file_path, 'w') as output_file:
             for line in input_file:
-                output_file.write(query_expander.find_most_similar_words(line.rstrip('\n').lower().split(' '),
-                                                                         number_of_additional_words))
+                expansions = query_expander.find_most_similar_words(line.rstrip('\n').lower().split(' '),
+                                                                    number_of_additional_words)
+                output_file.write(line.rstrip('\n'))
+                for word, _ in expansions:
+                    output_file.write(f' {word.decode()}')
+                output_file.write('\n')
 
 cli.add_command(generate_centroids)
 cli.add_command(expand)
